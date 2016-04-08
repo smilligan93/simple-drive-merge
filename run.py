0b0028eeca6b0cdae54b18ff0bc28988858b74ce
Shawn Milligan <smilligan93@gmail.com>
2016-04-08 16:41:26 -0400

    fuck everything again?

--- run.py
+++ run.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
-from os.path import join
+from os.path import join, abspath, islink
 import sys
 import getopt
 
-def symlink(src, dest):
+def symlink(src, dest, dir_only, file_only):
     """Will add all files in src to dest through 
     symlinks. If the item src exists as dest, it
     will drop into the directories and recursively
@@ -19,22 +19,51 @@ def symlink(src, dest):
     """
     for s_path, s_dirs, s_files in os.walk(src):
         for d_path, d_dirs, d_files in os.walk(dest):
-            for s_dir in s_dirs:
-                if s_dir in d_dirs:
-                    symlink(join(s_path,s_dir),join(d_path,s_dir))
-                else:
-                    print("would symlink " + join(s_path,s_dir) + " to " \
-                            join(d_path,s_dir))
-        pass
-    print(src + " " + dest)
-    pass
+            if not file_only:
+                for s_dir in s_dirs:
+                    if islink(join(d_path,s_dir)):
+                        break
+                    if s_dir in d_dirs:
+                        symlink(join(s_path,s_dir),
+                                join(d_path,s_dir),
+                                dir_only,
+                                file_only)
+                    else:
+                        abs_src_path  = abspath(join(s_path,s_dir))
+                        abs_dest_path = abspath(join(d_path,s_dir))
+                        if islink(abs_dest_path):
+                            break
+                        print("would symlink "
+                                + str(abs_src_path)
+                                + " to "
+                                + str(abs_dest_path))
+                        os.symlink(abs_src_path, abs_dest_path)
+                        #return True
+            break
+            if not dir_only:
+                for s_file in s_files:
+                    if islink(join(s_path,s_file)):
+                        pass
+                    elif s_file in d_files:
+                        pass
+                    else:
+                        abs_src_path  = abspath(join(s_path,s_file))
+                        abs_dest_path = abspath(join(d_path,s_file))
+                        print("would symlink "
+                                + str(abs_src_path)
+                                + " to "
+                                + str(abs_dest_path))
+                        #os.symlink(abs_src_path, abs_dest_path)
 
 
 if __name__ == "__main__":
+
+    dir_only = False
+    file_only = False
     try:
         # Short option syntax: "hv:"
         # Long option syntax: "help" or "verbose="
-        opts, args = getopt.getopt(sys.argv[1:], "hs:d:", ["src","dest"])
+        opts, args = getopt.getopt(sys.argv[1:], "hs:d:", ["src=","dest=","dir","file"])
     
     except(getopt.GetoptError, err):
         # Print debug info
@@ -50,5 +79,9 @@ if __name__ == "__main__":
             src = arg
         elif opt in ["-d", "--dest"]:
             dest = arg
+        elif opt in ["--dir"]:
+            dir_only = True
+        elif opt in ["--file"]:
+            file_only = True
 
-    symlink(src, dest)
+    symlink(src, dest, dir_only, file_only)
